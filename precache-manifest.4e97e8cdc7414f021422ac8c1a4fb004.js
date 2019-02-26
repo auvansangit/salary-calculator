5e66ca11805dfc4482916b937c2284a8d4f4ec8e
Sang Au <auvansang@outlook.com>
2019-02-26 13:45:30 +0700

    Updates

---
+++ precache-manifest.4e97e8cdc7414f021422ac8c1a4fb004.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "daef018d4935c364262d",
+    "url": "/salary-calculator/static/js/runtime~main.daef018d.js"
+  },
+  {
+    "revision": "6225098cb17fe0c33214",
+    "url": "/salary-calculator/static/js/main.6225098c.chunk.js"
+  },
+  {
+    "revision": "e8b9b815513178be6653",
+    "url": "/salary-calculator/static/js/6.e8b9b815.chunk.js"
+  },
+  {
+    "revision": "925a3e6b140abc84e3f0",
+    "url": "/salary-calculator/static/js/5.925a3e6b.chunk.js"
+  },
+  {
+    "revision": "0fefe8cad8f20d50a75d",
+    "url": "/salary-calculator/static/js/4.0fefe8ca.chunk.js"
+  },
+  {
+    "revision": "e5cf85ba90c4f8f19ae7",
+    "url": "/salary-calculator/static/js/3.e5cf85ba.chunk.js"
+  },
+  {
+    "revision": "3886d86a25475f5c6da9",
+    "url": "/salary-calculator/static/js/0.3886d86a.chunk.js"
+  },
+  {
+    "revision": "6225098cb17fe0c33214",
+    "url": "/salary-calculator/static/css/main.0ce296fc.chunk.css"
+  },
+  {
+    "revision": "582ce8b169c4264ecbe7ed81da2ddb64",
+    "url": "/salary-calculator/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "https://auvansang.github.io/salary-calculator/precache-manifest.9f7d0206434af690e72bb3f653315b72.js"
+  "/salary-calculator/precache-manifest.4e97e8cdc7414f021422ac8c1a4fb004.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("https://auvansang.github.io/salary-calculator/index.html", {
+workbox.routing.registerNavigationRoute("/salary-calculator/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
