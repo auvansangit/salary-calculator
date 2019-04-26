ffcfca7751ea19d5362de915aaf21dff879e9913
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-04-26 02:58:09 +0000

    Deploy auvansang/salary-calculator to github.com/auvansang/salary-calculator.git:gh-pages

---
+++ precache-manifest.eb38496a36f6336a73f75745ae7c16b3.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f602546f398fa6f30ba4dc7eed82dbf",
+    "url": "/salary-calculator/index.html"
+  },
+  {
+    "revision": "8f9e4ae4d744c5dd189e",
+    "url": "/salary-calculator/static/css/main.0ce296fc.chunk.css"
+  },
+  {
+    "revision": "555164bdffc9a18d15a7",
+    "url": "/salary-calculator/static/js/0.0c27aa97.chunk.js"
+  },
+  {
+    "revision": "ce7a92b2763f49535add",
+    "url": "/salary-calculator/static/js/3.b1d8c1ef.chunk.js"
+  },
+  {
+    "revision": "85f5020450d964a0f922",
+    "url": "/salary-calculator/static/js/4.ebedfa44.chunk.js"
+  },
+  {
+    "revision": "557af7c6a1c1cb4f21ad",
+    "url": "/salary-calculator/static/js/5.f123c974.chunk.js"
+  },
+  {
+    "revision": "9907e63f91a53240388b",
+    "url": "/salary-calculator/static/js/6.5bff3398.chunk.js"
+  },
+  {
+    "revision": "8f9e4ae4d744c5dd189e",
+    "url": "/salary-calculator/static/js/main.8480faf3.chunk.js"
+  },
+  {
+    "revision": "a7fbc0a0ea98e33a8468",
+    "url": "/salary-calculator/static/js/runtime~main.3ee0a2a7.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.0/workbox-sw.js");
 
 importScripts(
-  "/salary-calculator/precache-manifest.87aa2272cd4d8072e20d5011fc0fac35.js"
+  "/salary-calculator/precache-manifest.eb38496a36f6336a73f75745ae7c16b3.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/salary-calculator/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/salary-calculator/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
